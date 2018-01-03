23c6f52a7e732f1883f36183f4feb6e68eeafbda
Paul <ishika.paul@cgi.com>
2018-01-03 13:41:22 +0530

    added functionality to the reusable component

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter , OnInit } from '@angular/core';
 import { ProductTable} from '../../ProductTable';
+import { ChangePurchaseService} from '../../change-purchase.service';
+
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
@@ -8,14 +10,32 @@ import { ProductTable} from '../../ProductTable';
 export class ProductCardComponent implements OnInit {
    @Input('product') product:ProductTable;
    @Input('purchaseStatus') type:Boolean;
+   
 
-  constructor() { }
+  constructor( private object: ChangePurchaseService) { }
 
   ngOnInit() {
 
     // this.product.productName='Surf Excel';
     // this.product.price=56;
     
+     
+
+   }
+
+    change(pro: ProductTable) {
+    pro.purchase = false;
+    this.object.change(pro);
   }
 
-}
+  change1(pro: ProductTable) {
+    pro.purchase = true;
+    this.object.change1(pro);
+  }
+
+      
+    }
+    
+  
+
+
